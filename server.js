92e158271dd5273d5bcd124f0afda8c667471f55
evakshu <singhdurgeshpratap14@gmail.com>
2025-06-19 02:47:06 -0300

    Update server.js Final Run

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const MONGO_URI = process.env.DATABASE_URL;
 
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
